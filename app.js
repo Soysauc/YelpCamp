f6d73cf8c2c956949a86e5b4dabd11b270712b8a
Jeremy M Rivera <fangman09@gmail.com>
2023-01-09 00:07:53 -0500

    updates

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
-
+const dbUrl = "mongodb://localhost:27017/yelp-camp";
+//process.env.DB_URL ||
 mongoose.connect(dbUrl, {});
 
 const db = mongoose.connection;
